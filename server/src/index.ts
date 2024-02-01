8d32f7b87c707c2f0353ed4680b50b2cc5748960
Aditya <code.adi8103@gmail.com>
2024-02-01 15:04:25 +0530

    server commit

---
+++ server/src/index.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import {PrismaClient} from '@prisma/client'
+
+const prisma = new PrismaClient();
+const app = express()
+const PORT = 3000;
+
+app.use(express.json())
+
+app.listen(PORT,()=>{
+    console.log(`Server is running at port ${PORT}`);
+})
